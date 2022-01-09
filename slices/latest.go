4a10be687cd7011b947eeab5c869d683d6f3ccd4
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-01-09 12:21:50 +0330

    🔧 refactor: test case names and some benchmark variables updated, some styles updated

--- slices/latest.go
+++ slices/latest.go
@@ -3,20 +3,21 @@ package slices
 import (
 	"errors"
 	"reflect"
+
+	"github.com/gotorn/godash/internal"
 )
 
 func Latest(slice interface{}) (interface{}, error) {
-
-	if reflect.ValueOf(slice).Kind() != reflect.Slice {
-		return nil, errors.New("not a slice")
+	err := internal.SliceCheck(slice)
+	if err != nil {
+		return nil, err
 	}
 
-	len := reflect.ValueOf(slice).Len()
+	s := reflect.ValueOf(slice)
 
-	if len == 0 {
+	if s.Len() == 0 {
 		return nil, errors.New("slice is empty")
 	}
 
-	return reflect.ValueOf(slice).Index(len - 1).Interface(), nil
-
+	return s.Index(0).Interface(), nil
 }
