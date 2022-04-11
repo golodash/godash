ea478f7c05a569d1e129252b4c33186ebb17a023
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-04-11 11:19:59 +0430

    🎉 feat(slices-drop_right): drop_right function added

---
+++ slices/drop_right.go
@@ -0,0 +1,36 @@
+package slices
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/golodash/godash/internal"
+)
+
+// Creates a sub slice from a slice with n elements
+// dropped from the end.
+//
+// By default n is equal to 0
+func DropRight(slice interface{}, n ...int) ([]interface{}, error) {
+	var err error = internal.SliceCheck(slice)
+	if err != nil {
+		return nil, err
+	}
+
+	s := reflect.ValueOf(slice)
+	var to int = 0
+	if len(n) > 0 {
+		to = n[0]
+	}
+
+	if s.Len() < to {
+		return nil, errors.New("num is bigger than slice length")
+	}
+
+	output, err := internal.InterfaceToSlice(s.Slice(0, s.Len()-to).Interface())
+	if err != nil {
+		return nil, err
+	}
+
+	return output, err
+}
