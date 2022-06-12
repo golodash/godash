01242b764694e8e6301e707ab72091d10b733bf5
amirhz <amirhz9978@gmail.com>
2022-06-12 20:51:05 +0430

    🎉 feat(slices-xor_by): xor_by function added

---
+++ slices/xor_by.go
@@ -0,0 +1,38 @@
+package slices
+
+import (
+	"reflect"
+
+	"github.com/golodash/godash/internal"
+)
+
+func XorBy(function func(interface{}, interface{}) bool, slices ...interface{}) (interface{}, error) {
+	var length int = 0
+	for i := 0; i < len(slices); i++ {
+		if err := internal.SliceCheck(slices[i]); err != nil {
+			return nil, err
+		}
+		length += reflect.ValueOf(slices[i]).Len()
+	}
+
+	unSeenItems := make([]interface{}, 0, length)
+	var sCheck bool
+	for i := 0; i < len(slices); i++ {
+		item := reflect.ValueOf(slices[i])
+		for j := 0; j < item.Len(); j++ {
+			sCheck = true
+			for sIndex, s := range unSeenItems {
+				if function(item.Index(j).Interface(), s) {
+					unSeenItems = append(unSeenItems[:sIndex], unSeenItems[sIndex+1:]...)
+					sCheck = false
+					break
+				}
+			}
+			if sCheck {
+				unSeenItems = append(unSeenItems, item.Index(j).Interface())
+			}
+		}
+	}
+
+	return unSeenItems, nil
+}
