920bc5fbba06f1cb017dd0f2f13904392d7a37ba
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-06-17 12:41:42 +0430

    🔧 refactor(slices-zip_by): just refactored

--- slices/zip_by.go
+++ slices/zip_by.go
@@ -1,61 +1,61 @@
 package slices
 
 import (
-	"errors"
 	"reflect"
 
 	"github.com/golodash/godash/internal"
 )
 
-func ZipBy(function interface{}, slices ...interface{}) (interface{}, error) {
-	var typeOfSlices reflect.Type = nil
-	var sizeOfAllSlices int = 0
-	var maxLength int = -1
-	for i := 0; i < len(slices); i++ {
-		if err := internal.SliceCheck(slices[i]); err != nil {
+// This method is like Zip except that it accepts a function to specify
+// how grouped values should be combined.
+//
+// Complexity: O(n)
+func ZipBy(slices interface{}, function func(interface{}) interface{}) (interface{}, error) {
+	if err := internal.SliceCheck(slices); err != nil {
+		return nil, err
+	}
+
+	// Calculating output type
+	var output = reflect.Value{}
+	sliceItemType := reflect.TypeOf(slices)
+	slicesValue := reflect.ValueOf(slices)
+	for sliceItemType.Kind() == reflect.Slice {
+		sliceItemType = sliceItemType.Elem()
+	}
+	if sliceItemType.Kind() != reflect.Interface {
+		output = reflect.MakeSlice(reflect.TypeOf(slices).Elem(), 0, slicesValue.Len()/2)
+	} else {
+		output = reflect.MakeSlice(reflect.TypeOf([][]interface{}{}), 0, slicesValue.Len()/2)
+	}
+
+	// Calculating length
+	sizeOfAllSlices := 0
+	maxLength := -1
+	for i := 0; i < slicesValue.Len(); i++ {
+		item := slicesValue.Index(i).Interface()
+		itemValue := reflect.ValueOf(item)
+		if err := internal.SliceCheck(item); err != nil {
 			return nil, err
 		}
-		if typeOfSlices == nil {
-			typeOfSlices = reflect.TypeOf(slices[i])
+		if maxLength < itemValue.Len() {
+			maxLength = itemValue.Len()
 		}
-		if typeOfSlices != reflect.TypeOf(slices[i]) {
-			typeOfSlices = reflect.TypeOf([]interface{}{})
-		}
-		if maxLength == -1 {
-			maxLength = reflect.ValueOf(slices[i]).Len()
-		}
-		if maxLength < reflect.ValueOf(slices[i]).Len() {
-			maxLength = reflect.ValueOf(slices[i]).Len()
-		}
-		sizeOfAllSlices += reflect.ValueOf(slices[i]).Len()
-	}
-	if typeOfSlices == nil {
-		typeOfSlices = reflect.TypeOf([]interface{}{})
+		sizeOfAllSlices += itemValue.Len()
 	}
 
-	functionValue := reflect.ValueOf(function)
-	if functionValue.Kind() != reflect.Func {
-		return nil, errors.New("'function' variable has to be 'function' type")
-	}
-	if functionValue.Type().NumOut() != 1 {
-		return nil, errors.New("number of 'function' output has to be one")
-	}
-	if functionValue.Type().In(0).Kind() != reflect.Slice && functionValue.Type().In(0).Elem().String() != typeOfSlices.Elem().String() && functionValue.Type().In(0).Kind() != reflect.Interface {
-		return nil, errors.New("number of 'function' input has to be one and be compatible with inputs slices elements type")
-	}
-
-	output := reflect.MakeSlice(reflect.SliceOf(functionValue.Type().Out(0)), 0, sizeOfAllSlices)
-	functionInput := []reflect.Value{}
+	functionInput := reflect.MakeSlice(reflect.TypeOf(slices).Elem(), 0, 0)
 	for j := 0; j < maxLength; j++ {
-		for i := 0; i < len(slices); i++ {
-			subSlice := reflect.ValueOf(slices[i])
+		for i := 0; i < slicesValue.Len(); i++ {
+			subSlice := reflect.ValueOf(slicesValue.Index(i).Interface())
 			if j >= subSlice.Len() {
 				continue
 			}
-			functionInput = append(functionInput, subSlice.Index(j))
+			functionInput = reflect.Append(functionInput, subSlice.Index(j))
 		}
-		output = reflect.Append(output, functionValue.Call(functionInput)[0])
-		functionInput = []reflect.Value{}
+		funcOutput := reflect.ValueOf(function(functionInput.Interface()))
+
+		output = reflect.Append(output, funcOutput)
+		functionInput = reflect.Zero(functionInput.Type())
 	}
 
 	return output.Interface(), nil
--- slices/zip_by_test.go
+++ slices/zip_by_test.go
@@ -10,8 +10,8 @@ import (
 
 type TZipBy struct {
 	name string
-	arr  [][]int
-	want []int
+	arr  interface{}
+	want interface{}
 }
 
 var tZipByBenchs = []TZipBy{
@@ -41,15 +41,16 @@ func init() {
 	for j := 0; j < len(tZipByBenchs); j++ {
 		length, _ := strconv.Atoi(tZipByBenchs[j].name)
 		for i := 0; i < length/10; i++ {
-			tZipByBenchs[j].arr = append(tZipByBenchs[j].arr, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+			tZipByBenchs[j].arr = append(tZipByBenchs[j].arr.([][]int), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
 		}
 	}
 }
 
-func returnZipByTest(inputs ...interface{}) int {
+func returnZipByTest(inputs interface{}) interface{} {
 	output := 0
-	for i := 0; i < len(inputs); i++ {
-		output += inputs[i].(int)
+	literalInputs := inputs.([]int)
+	for i := 0; i < len(literalInputs); i++ {
+		output += literalInputs[i]
 	}
 
 	return output
@@ -86,23 +87,18 @@ func TestZipBy(t *testing.T) {
 
 	for _, subject := range tests {
 		t.Run(subject.name, func(t *testing.T) {
-			inputs := []interface{}{}
-			for i := 0; i < len(subject.arr); i++ {
-				var temp interface{} = subject.arr[i]
-				inputs = append(inputs, temp)
-			}
-			got, err := ZipBy(returnZipByTest, inputs...)
+			got, err := ZipBy(subject.arr, returnZipByTest)
 
 			if err != nil {
 				if subject.want != nil {
-					t.Errorf("ZipBy() got = %v, wanted = %v, error = %v", got, subject.want, err)
+					t.Errorf("got = %v, wanted = %v, err = %v", got, subject.want, err)
 				}
 				return
 			}
 
 			if _, err := internal.Same(got, subject.want); err != nil {
-				t.Errorf("ZipBy() got = %v, wanted = %v, error = %v", got, subject.want, err)
-
+				t.Errorf("got = %v, wanted = %v, err = %v", got, subject.want, err)
+				return
 			}
 		})
 	}
@@ -110,14 +106,9 @@ func TestZipBy(t *testing.T) {
 
 func BenchmarkZipBy(b *testing.B) {
 	for j := 0; j < len(tZipByBenchs); j++ {
-		inputs := []interface{}{}
-		for i := 0; i < len(tZipByBenchs[j].arr); i++ {
-			var temp interface{} = tZipByBenchs[j].arr[i]
-			inputs = append(inputs, temp)
-		}
 		b.Run(fmt.Sprintf("slice_size_%s", tZipByBenchs[j].name), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				ZipBy(returnZipByTest, inputs...)
+				ZipBy(tZipByBenchs[j].arr, returnZipByTest)
 			}
 		})
 	}
