9bbdc29632e00eefb14619912f18fd86eff63abc
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-11-07 16:13:04 +0330

    ✨ style: just some text changed

---
+++ functions/once_test.go
@@ -0,0 +1,14 @@
+package functions
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOnce(t *testing.T) {
+	fmt.Println("I don't think any test cases can be written for Once")
+}
+
+func BenchmarkOnce(b *testing.B) {
+	fmt.Println("I don't think any benchmarks can be written for Once")
+}
